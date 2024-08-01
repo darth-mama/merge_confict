b378bcc85772476ebb2789fc48f31f2acfc4a581
darth-mama <28187728+darth-mama@users.noreply.github.com>
2024-08-01 18:40:33 -0500

    original commit

---
+++ math.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  return a / b;
+}
+
+const add_res = add(3, 5);
+console.log(add_res); //outputs 8
