dc6b99b49ecf634360f3bf833b7e7f0b8359f8ab
darth-mama <28187728+darth-mama@users.noreply.github.com>
2024-08-01 19:08:19 -0500

    added exponents

---
+++ math.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  return a / b;
+}
+
+function exponent(base, exponent) {
+  return base ** exponent;
+}
+
+const exp_result = exponent(2, 3);
+console.log(result); //Outputs: 8
